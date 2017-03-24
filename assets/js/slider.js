073d027c7d237c07cb81c85171e48b045e2dec64
WaelMohammedAbed <waelrealmadrid@gmail.com>
2017-03-24 22:52:38 +0300

    add video gallery to gallery page
    add video samples need to be change to something related to website
    subject

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -8,6 +8,17 @@ function next(){
 i=mod(i+1,images.length);
 document.getElementById("slider").src=images[i];
 }
+
+var videos = ["https://www.youtube.com/embed/zZU_5gFGorU","https://www.youtube.com/embed/X97-eh612oA","https://www.youtube.com/embed/Qy7C5Ifu_Xw","https://www.youtube.com/embed/_9jEOr6VA0A"];
+var j=0;
+function Vprev(){
+j=mod(j-1,videos.length);
+document.getElementById("Vslider").src=videos[j];
+}
+function Vnext(){
+j=mod(j+1,videos.length);
+document.getElementById("Vslider").src=videos[j];
+}
 function mod(n, m) {
         return ((n % m) + m) % m;
 }
